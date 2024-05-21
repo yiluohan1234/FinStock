90442fc401ef9f01c53863f6027da9f74d241020
luojiahan <cuiyufei1234@126.com>
2024-05-21 10:39:52 +0800

    提高数据生成效率

--- utils/Basic.py
+++ utils/Basic.py
@@ -8,6 +8,8 @@
 #    > description: 基本面分析
 #######################################################################
 import akshare as ak
+import pandas as pd
+
 from utils.func import get_szsh_code, str2value, get_display_data, get_report_type
 
 
@@ -49,12 +51,19 @@ def get_basic_info(code, indicator='2023-12-31'):
     return df[ret_columns]
 
 
-def get_main_indicators_ths(code, n, indicator="按年度"):
-    '''获取股票代码带字母的
-    @params:
-    - code: str      # 股票代码
-    - n: str         # 返回数据个数
-    - indicator: str # 数据类型，indicator="按报告期"; choice of {"按报告期", "按年度", "按单季度"}
+def get_main_indicators_ths(code, n, indicator="按年度", is_display=True):
+    '''
+    获取主要指标数据
+    :param code: 股票代码
+    :type code: str
+    :param n: 返回数据个数
+    :type n: str
+    :param indicator: 数据类型，indicator="按报告期"; choice of {"按报告期", "按年度", "按单季度"}
+    :type indicator: str
+    :param is_display: 是否返回展示数据
+    :type is_display: bool
+    :return: 返回主要指标数据
+    :rtype: pandas.DataFrame
     '''
     # indicator="按报告期"; choice of {"按报告期", "按年度", "按单季度"}
     df = ak.stock_financial_abstract_ths(symbol=code, indicator=indicator)
@@ -67,15 +76,28 @@ def get_main_indicators_ths(code, n, indicator="按年度"):
     df['报告期'] = df['报告期'].astype("str")
 
     df_display = get_display_data(df)
-    return df, df_display
+
+    if is_display:
+        return df_display
+    else:
+        return df
 
 
-def get_main_indicators_sina(code, n, indicator="按年度", ret_columns=[]):
-    '''获取股票代码带字母的
-    @params:
-    - code: str      # 股票代码
-    - n: str         # 返回数据个数
-    - indicator: str # 数据类型，indicator="按报告期"; choice of {"按报告期", "按年度", "按单季度"}
+def get_main_indicators_sina(code, n, indicator="按年度", ret_columns=[], is_display=True):
+    '''
+    获取主要指标数据
+    :param code: 股票代码
+    :type code: str
+    :param n: 返回数据个数
+    :type n: str
+    :param indicator: 数据类型，indicator="按报告期"; choice of {"按报告期", "按年度", "按单季度"}
+    :type indicator: int
+    :param ret_columns: 返回数据列名列表
+    :type ret_columns: list
+    :param is_display: 是否返回展示数据
+    :type is_display: bool
+    :return: 返回主要指标数据
+    :rtype: pandas.DataFrame
     '''
     df = ak.stock_financial_abstract(code)
     df.drop(['选项'] ,axis=1, inplace=True)
@@ -100,163 +122,213 @@ def get_main_indicators_sina(code, n, indicator="按年度", ret_columns=[]):
 
     df_display = get_display_data(df)
     df_display.drop_duplicates(subset=df_display.columns.tolist(), keep='first', inplace=True)
-    return df, df_display
+    if is_display:
+        return df_display
+    else:
+        return df
 
 
-def get_lrb_data(code, n, data_type=0):
-    '''获取利润表数据
-    @params:
-    - code: str        #代码
-    - n: int           #返回数据个数
-    - data_type: int   #返回数据的形式，0为自然，1为季报，2为中报，4为年报
+def get_lrb_data(code, n, data_type=0, is_display=True):
     '''
-    ret_columns = ['报告日', '营业总收入', '营业总收入同比',
-                   '营业总成本', '营业总成本同比',
-                   '营业利润', '营业利润同比',
-                   '利润总额', '利润总额同比',
-                   '净利润', '净利润同比',
-                   '归属于母公司所有者的净利润', '归属于母公司所有者的净利润同比',
-                   '销售毛利率', '销售净利率', '销售成本率',
-                   '净利润/营业总收入(%)', '营业利润/营业总收入(%)', '息税前利润/营业总收入(%)',
-                   '营业总成本/营业总收入(%)', '销售费用/营业总收入(%)',
-                   '管理费用/营业总收入(%)', '管理费用(含研发费用)/营业总收入(%)',
-                   '财务费用/营业总收入(%)', '研发费用/营业总收入(%)']
+    获取利润表数据
+    :param code: 股票代码
+    :type code: str
+    :param n: 返回数据个数
+    :type n: str
+    :param data_type: 返回数据的形式，0为自然，1为季报，2为中报，4为年报
+    :type data_type: int
+    :param is_display: 是否返回展示数据
+    :type is_display: bool
+    :return: 返回利润表数据
+    :rtype: pandas.DataFrame
+    '''
     # df_lrb = ak.stock_financial_report_sina(stock=self.code, symbol='利润表')
     df_lrb = ak.stock_profit_sheet_by_report_em(symbol=get_szsh_code(code))
-    df_lrb = df_lrb.fillna(0)
 
+    dt = {}
     # 过滤年报
-    df_lrb['报告日'] = df_lrb['REPORT_DATE_NAME']
+    dt['报告日'] = df_lrb['REPORT_DATE_NAME']
     # df_lrb = df_lrb.sort_index(ascending=False)
     # 营业收入及同比
-    df_lrb['营业总收入'] = round(df_lrb['TOTAL_OPERATE_INCOME'] / 100000000, 2)
-    df_lrb['营业总收入同比'] = round(df_lrb['TOTAL_OPERATE_INCOME_YOY'], 2)
-    df_lrb['营业总成本'] = round(df_lrb['TOTAL_OPERATE_COST'] / 100000000, 2)
-    df_lrb['营业总成本同比'] = round(df_lrb['TOTAL_OPERATE_COST_YOY'], 2)
-    df_lrb['营业利润'] = round(df_lrb['OPERATE_PROFIT'] / 100000000, 2)
-    df_lrb['营业利润同比'] = round(df_lrb['OPERATE_PROFIT_YOY'], 2)
-    df_lrb['利润总额'] = round(df_lrb['TOTAL_PROFIT'] / 100000000, 2)
-    df_lrb['利润总额同比'] = round(df_lrb['TOTAL_PROFIT_YOY'], 2)
-    df_lrb['净利润'] = round(df_lrb['NETPROFIT'] / 100000000, 2)
-    df_lrb['净利润同比'] = round(df_lrb['NETPROFIT_YOY'], 2)
-    df_lrb['归属于母公司所有者的净利润'] = round(df_lrb['PARENT_NETPROFIT'] / 100000000, 2)
-    df_lrb['归属于母公司所有者的净利润同比'] = round(df_lrb['PARENT_NETPROFIT_YOY'], 2)
+    dt['营业总收入'] = round(df_lrb['TOTAL_OPERATE_INCOME'] / 100000000, 2)
+    dt['营业总收入同比'] = round(df_lrb['TOTAL_OPERATE_INCOME_YOY'], 2)
+    dt['营业总成本'] = round(df_lrb['TOTAL_OPERATE_COST'] / 100000000, 2)
+    dt['营业总成本同比'] = round(df_lrb['TOTAL_OPERATE_COST_YOY'], 2)
+    dt['营业利润'] = round(df_lrb['OPERATE_PROFIT'] / 100000000, 2)
+    dt['营业利润同比'] = round(df_lrb['OPERATE_PROFIT_YOY'], 2)
+    dt['利润总额'] = round(df_lrb['TOTAL_PROFIT'] / 100000000, 2)
+    dt['利润总额同比'] = round(df_lrb['TOTAL_PROFIT_YOY'], 2)
+    dt['净利润'] = round(df_lrb['NETPROFIT'] / 100000000, 2)
+    dt['净利润同比'] = round(df_lrb['NETPROFIT_YOY'], 2)
+    dt['归属于母公司所有者的净利润'] = round(df_lrb['PARENT_NETPROFIT'] / 100000000, 2)
+    dt['归属于母公司所有者的净利润同比'] = round(df_lrb['PARENT_NETPROFIT_YOY'], 2)
     # 净利润/营业总收入(%)
     # 营业利润/营业总收入(%)
     # 息税前利润/营业总收入(%)
     # EBITDA/营业总收入(%)
 
-    df_lrb['净利润/营业总收入(%)'] = round(df_lrb['NETPROFIT'] * 100 / df_lrb['TOTAL_OPERATE_INCOME'], 2)
-    df_lrb['营业利润/营业总收入(%)'] = round(df_lrb['OPERATE_PROFIT'] * 100 / df_lrb['TOTAL_OPERATE_INCOME'], 2)
+    dt['净利润/营业总收入(%)'] = round(df_lrb['NETPROFIT'] * 100 / df_lrb['TOTAL_OPERATE_INCOME'], 2)
+    dt['营业利润/营业总收入(%)'] = round(df_lrb['OPERATE_PROFIT'] * 100 / df_lrb['TOTAL_OPERATE_INCOME'], 2)
     # 息税前利润 （EBTI）= 净利润 + 财务费用 + 所得税费用
-    df_lrb['息税前利润/营业总收入(%)'] = round(
+    dt['息税前利润/营业总收入(%)'] = round(
         (df_lrb['NETPROFIT'] + df_lrb['FINANCE_EXPENSE'] + df_lrb['INCOME_TAX']) * 100 / df_lrb[
             'TOTAL_OPERATE_INCOME'], 2)
-    df_lrb['营业总成本/营业总收入(%)'] = round(df_lrb['TOTAL_OPERATE_COST'] * 100 / df_lrb['TOTAL_OPERATE_INCOME'], 2)
-    df_lrb['销售费用/营业总收入(%)'] = round(df_lrb['SALE_EXPENSE'] * 100 / df_lrb['TOTAL_OPERATE_INCOME'], 2)
-    df_lrb['管理费用/营业总收入(%)'] = round(df_lrb['MANAGE_EXPENSE'] * 100 / df_lrb['TOTAL_OPERATE_INCOME'], 2)
-    df_lrb['管理费用(含研发费用)/营业总收入(%)'] = round(
+    dt['营业总成本/营业总收入(%)'] = round(df_lrb['TOTAL_OPERATE_COST'] * 100 / df_lrb['TOTAL_OPERATE_INCOME'], 2)
+    dt['销售费用/营业总收入(%)'] = round(df_lrb['SALE_EXPENSE'] * 100 / df_lrb['TOTAL_OPERATE_INCOME'], 2)
+    dt['管理费用/营业总收入(%)'] = round(df_lrb['MANAGE_EXPENSE'] * 100 / df_lrb['TOTAL_OPERATE_INCOME'], 2)
+    dt['管理费用(含研发费用)/营业总收入(%)'] = round(
         (df_lrb['MANAGE_EXPENSE'] + df_lrb['RESEARCH_EXPENSE']) * 100 / df_lrb['TOTAL_OPERATE_INCOME'], 2)
-    df_lrb['财务费用/营业总收入(%)'] = round(df_lrb['FINANCE_EXPENSE'] * 100 / df_lrb['TOTAL_OPERATE_INCOME'], 2)
-    df_lrb['研发费用/营业总收入(%)'] = round(df_lrb['RESEARCH_EXPENSE'] * 100 / df_lrb['TOTAL_OPERATE_INCOME'], 2)
+    dt['财务费用/营业总收入(%)'] = round(df_lrb['FINANCE_EXPENSE'] * 100 / df_lrb['TOTAL_OPERATE_INCOME'], 2)
+    dt['研发费用/营业总收入(%)'] = round(df_lrb['RESEARCH_EXPENSE'] * 100 / df_lrb['TOTAL_OPERATE_INCOME'], 2)
     # 销售毛利率=销售毛利/销售收入×100%=（销售收入-销售成本）/销售收入×100%= (营业收入 - 营业成本 / 营业收入) * 100%
-    df_lrb['销售毛利率'] = round((df_lrb['OPERATE_INCOME'] - df_lrb['OPERATE_COST']) * 100 / df_lrb['OPERATE_INCOME'], 2)
+    dt['销售毛利率'] = round((df_lrb['OPERATE_INCOME'] - df_lrb['OPERATE_COST']) * 100 / df_lrb['OPERATE_INCOME'], 2)
     # 净利率=净利润/营业收入
-    df_lrb['销售净利率'] = round(df_lrb['NETPROFIT'] * 100 / df_lrb['OPERATE_INCOME'], 2)
+    dt['销售净利率'] = round(df_lrb['NETPROFIT'] * 100 / df_lrb['OPERATE_INCOME'], 2)
     # 销售成本率=销售成本/销售收入净额×100%
-    df_lrb['销售成本率'] = round(df_lrb['OPERATE_COST'] * 100 / df_lrb['OPERATE_INCOME'], 2)
+    dt['销售成本率'] = round(df_lrb['OPERATE_COST'] * 100 / df_lrb['OPERATE_INCOME'], 2)
+    ret_df = pd.DataFrame(dt)
+    ret_df = ret_df.fillna(0)
 
     if data_type == 1:
-        df_lrb = df_lrb[df_lrb['REPORT_TYPE'] == '一季报']
+        ret_df = ret_df[ret_df['REPORT_TYPE'] == '一季报']
     elif data_type == 2:
-        df_lrb = df_lrb[df_lrb['REPORT_TYPE'] == '中报']
+        ret_df = ret_df[ret_df['REPORT_TYPE'] == '中报']
     elif data_type == 4:
-        df_lrb = df_lrb[df_lrb['REPORT_TYPE'] == '年报']
+        ret_df = ret_df[ret_df['REPORT_TYPE'] == '年报']
 
     # 返回最近n个数据
-    df_lrb = df_lrb[ret_columns].head(n)
-    df_lrb_display = get_display_data(df_lrb)
+    ret_df = ret_df.head(n)
+    ret_df_display = get_display_data(ret_df)
 
-    return df_lrb, df_lrb_display
+    if is_display:
+        return ret_df_display
+    else:
+        return ret_df
 
-def get_zcfz_data(code, n, data_type=0):
-    '''获取资产负债表数据
-    @params:
-    - n: int           #返回数据个数
-    - data_type: int   #返回数据的形式，0为自然，1为季报，2为中报，4为年报
+
+def get_zcfz_data(code, n, data_type=0, is_display=True):
     '''
-    ret_columns = ['报告日', '总资产', '总负债',
-                   '资产负债率']
+    获取资产负债表数据
+    :param code: 股票代码
+    :type code: str
+    :param n: 返回数据个数
+    :type n: str
+    :param data_type: 返回数据的形式，0为自然，1为季报，2为中报，4为年报
+    :type data_type: int
+    :param is_display: 是否返回展示数据
+    :type is_display: bool
+    :return: 返回现金流量数据
+    :rtype: pandas.DataFrame
+    '''
     df_zcfz = ak.stock_balance_sheet_by_report_em(symbol=get_szsh_code(code))
-    df_zcfz = df_zcfz.fillna(0)
     # https://blog.csdn.net/a389085918/article/details/80284812
-
-    df_zcfz['报告日'] = df_zcfz['REPORT_DATE_NAME']
+    dt = {}
+    dt['报告日'] = df_zcfz['REPORT_DATE_NAME']
     # df_zcfz = df_zcfz.sort_index(ascending=False)
     # 资产负债率
-    df_zcfz['总资产'] = round(df_zcfz['TOTAL_ASSETS'] / 100000000, 2)
-    df_zcfz['总负债'] = round(df_zcfz['TOTAL_LIABILITIES'] / 100000000, 2)
-    df_zcfz['资产负债率'] = round(df_zcfz['TOTAL_LIABILITIES'] * 100 / df_zcfz['TOTAL_ASSETS'], 2)
+    dt['总资产'] = round(df_zcfz['TOTAL_ASSETS'] / 100000000, 2)
+    dt['总负债'] = round(df_zcfz['TOTAL_LIABILITIES'] / 100000000, 2)
+    dt['资产负债率'] = round(df_zcfz['TOTAL_LIABILITIES'] * 100 / df_zcfz['TOTAL_ASSETS'], 2)
+    dt['存货'] = round(df_zcfz['INVENTORY'] / 100000000, 2)
+    ret_df = pd.DataFrame(dt)
+    ret_df = ret_df.fillna(0)
     # 应收账款周转率=营业收入/（（期初应收账款+期末应收账款）/2）
     # 应收账款周转天数=365/应收账款周转率
 
     if data_type == 1:
-        df_zcfz = df_zcfz[df_zcfz['REPORT_TYPE'] == '一季报']
+        ret_df = ret_df[ret_df['REPORT_TYPE'] == '一季报']
     elif data_type == 2:
-        df_zcfz = df_zcfz[df_zcfz['REPORT_TYPE'] == '中报']
+        ret_df = ret_df[ret_df['REPORT_TYPE'] == '中报']
     elif data_type == 4:
-        df_zcfz = df_zcfz[df_zcfz['REPORT_TYPE'] == '年报']
+        ret_df = ret_df[ret_df['REPORT_TYPE'] == '年报']
     # 返回最近n个数据
-    df_zcfz = df_zcfz[ret_columns].head(n)
+    ret_df = ret_df.head(n)
 
-    df_zcfz_display = get_display_data(df_zcfz)
-    return df_zcfz, df_zcfz_display
+    ret_df_display = get_display_data(ret_df)
+
+    if is_display:
+        return ret_df_display
+    else:
+        return ret_df
 
 
-def get_xjll_data(code, n, data_type=0):
-    '''获取现金流量表数据
-    @params:
-    - n: int           #返回数据个数
-    - data_type: int   #返回数据的形式，0为自然，1为季报，2为中报，4为年报
+def get_xjll_data(code, n, data_type=0, is_display=True):
     '''
-    ret_columns = ['报告日', '总资产', '总负债',
-                   '资产负债率']
+    获取现金流量表数据
+    :param code: 股票代码
+    :type code: str
+    :param n: 返回数据个数
+    :type n: str
+    :param data_type: 返回数据的形式，0为自然，1为季报，2为中报，4为年报
+    :type data_type: int
+    :param is_display: 是否返回展示数据
+    :type is_display: bool
+    :return: 返回现金流量数据
+    :rtype: pandas.DataFrame
+    '''
     # 人力投入回报率=企业净利润/员工薪酬福利总额×100%，这是衡量人力资本有效性的核心指标，表明公司在人力资源上的投入和净利润的比值，回报率越高，说明人力资源的效率和效能越高。
     df_xjll = ak.stock_cash_flow_sheet_by_report_em(symbol=get_szsh_code(code))
-    df_xjll['员工薪酬福利总额'] = round(df_xjll['PAY_STAFF_CASH']/100000000, 2)
-    df_xjll = df_xjll.fillna(0)
+    dt = {}
+    dt['报告日'] = df_xjll['REPORT_DATE_NAME']
+    dt['员工薪酬福利总额'] = round(df_xjll['PAY_STAFF_CASH']/100000000, 2)
+    ret_df = pd.DataFrame(dt)
+    ret_df = ret_df.fillna(0)
 
     if data_type == 1:
-        df_xjll = df_xjll[df_xjll['REPORT_TYPE'] == '一季报']
+        ret_df = ret_df[ret_df['REPORT_TYPE'] == '一季报']
     elif data_type == 2:
-        df_xjll = df_xjll[df_xjll['REPORT_TYPE'] == '中报']
+        ret_df = ret_df[ret_df['REPORT_TYPE'] == '中报']
     elif data_type == 4:
-        df_xjll = df_xjll[df_xjll['REPORT_TYPE'] == '年报']
+        ret_df = ret_df[ret_df['REPORT_TYPE'] == '年报']
     # 返回最近n个数据
-    df_xjll = df_xjll[ret_columns].head(n)
+    ret_df = ret_df.head(n)
 
-    df_xjll_display = get_display_data(df_xjll)
-    return df_xjll, df_xjll_display
+    ret_df_display = get_display_data(ret_df)
 
-def get_zygc_data(code):
-    '''获取主营构成数据
-    @params:
+    if is_display:
+        return ret_df_display
+    else:
+        return ret_df
+
+
+def get_zygc_data(code, data_date, indicator="全部"):
     '''
-    ret_columns = ['报告日期', '分类类型', '主营构成', '主营收入', '收入比例', '主营成本', '成本比例', '主营利润', '利润比例', '毛利率']
+    获取主营构成数据
+    :param code: 股票代码
+    :type code: str
+    :param data_date: 报告日期，'2023-12-31'
+    :type data_date: str
+    :param indicator: 分类类型，按产品分类，按地区分类，全部
+    :type indicator: str
+    :return: 返回主营构成数据
+    :rtype: pandas.DataFrame
+    '''
     # 主营构成-东财
     zygc_em_df = ak.stock_zygc_em(symbol=get_szsh_code(code))
-    zygc_em_df['主营收入'] = round(zygc_em_df['主营收入'] / 100000000, 2)
-    zygc_em_df['主营成本'] = round(zygc_em_df['主营成本'] / 100000000, 2)
-    zygc_em_df['主营利润'] = round(zygc_em_df['主营利润'] / 100000000, 2)
-    zygc_em_df['收入比例'] = round(zygc_em_df['收入比例'] * 100, 2)
-    zygc_em_df['成本比例'] = round(zygc_em_df['成本比例'] * 100, 2)
-    zygc_em_df['利润比例'] = round(zygc_em_df['利润比例'] * 100, 2)
-    zygc_em_df['毛利率'] = round(zygc_em_df['毛利率'] * 100, 2)
-    df = zygc_em_df[zygc_em_df['报告日期'].astype(str) == '2023-12-31']
-    df = df.sort_values(by=['分类类型', '收入比例'], ascending=[True, False])
+    zygc_em_df = zygc_em_df[zygc_em_df['报告日期'].astype(str) == data_date]
+    dt = {}
+    dt['报告日期'] = zygc_em_df['报告日期']
+    dt['分类类型'] = zygc_em_df['分类类型']
+    dt['主营收入'] = round(zygc_em_df['主营收入'] / 100000000, 2)
+    dt['收入比例'] = round(zygc_em_df['收入比例'] * 100, 2)
+    dt['主营成本'] = round(zygc_em_df['主营成本'] / 100000000, 2)
+    dt['成本比例'] = round(zygc_em_df['成本比例'] * 100, 2)
+    dt['主营利润'] = round(zygc_em_df['主营利润'] / 100000000, 2)
+    dt['利润比例'] = round(zygc_em_df['利润比例'] * 100, 2)
+    dt['毛利率'] = round(zygc_em_df['毛利率'] * 100, 2)
+    ret_df = pd.DataFrame(dt)
+    ret_df.reset_index()
+    ret_df = ret_df.sort_values(by=['分类类型', '收入比例'], ascending=[True, False])
+    if indicator != "全部":
+        ret_df = ret_df[ret_df['分类类型'] == indicator]
 
-    return df[ret_columns]
+    return ret_df
 
 if __name__ == "__main__":
-    df = get_basic_info("000737")
-    print(df)
+    code = "000737"
+    # df = get_basic_info("000737")
+    # print(df)
+    # zygc_em_df = get_zygc_data("000737", "2023-12-31", indicator="按产品分类")
+    # print(zygc_em_df)
+    df_xjll = get_xjll_data(code, 5, is_display=False)
+    print(df_xjll)
