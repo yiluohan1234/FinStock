9e19ed64f1b362b01ca9fc38d40853a9f2b03055
luojiahan <cuiyufei1234@126.com>
2024-05-27 14:59:19 +0800

    增加自定义数据列

--- strategies/sma_cross_stragegy.py
+++ strategies/sma_cross_stragegy.py
@@ -20,11 +20,16 @@ from utils.data import get_kline_chart_date
 from datetime import date, datetime
 
 class SmaCross(bt.Strategy):
-    params = (('pfast', 10), ('pslow', 30),)
+    params = (
+        ("printlog", False),
+    )
 
     def __init__(self):
-        sma1 = btind.SMA(period=self.p.pfast)
-        sma2 = btind.SMA(period=self.p.pslow)
+        self.data_close = self.datas[0].close  # 指定价格序列
+        # 初始化交易指令、买卖价格和手续费
+        self.order = None
+        self.buy_price = None
+        self.buy_comm = None
         self.k10 = self.datas[0].k10
         self.k10_pre = self.datas[-1].k10
         self.k20 = self.datas[0].k20
@@ -34,14 +39,61 @@ class SmaCross(bt.Strategy):
         self.crossover = btind.CrossUp(self.k10, self.k20)
 
     def next(self):
+        if self.order:  # 检查是否有指令等待执行
+            return
+        # 检查是否持仓
         if self.position.size == 0:
-            if self.crossover > 0 and self.k10 < 0 and self.k20 < 0 and self.k60 < 0 and self.k10 > self.k10_pre and self.k20 > self.k20_pre and self.k60 >= self.k60_pre:
+            if self.crossover > 0:# and self.k10 < 0 and self.k20 < 0 and self.k60 < 0 and self.k10 > self.k10_pre and self.k20 > self.k20_pre and self.k60 >= self.k60_pre:
                 amount_to_invest = (self.broker.cash * 0.95)
                 self.size = int(amount_to_invest / self.data.close)
-                self.buy(size=self.size)
+                self.log("BUY CREATE, %.2f" % self.data_close[0])
+                self.order = self.buy()
         elif self.position.size > 0:
-            if self.crossover < 0 and self.k10 > 0 and self.k20 > 0 and self.k60 > 0 and self.k10 < self.k10_pre and self.k20 < self.k20_pre and self.k60 <= self.k60_pre:
-                self.close()
+            if self.crossover < 0:# and self.k10 > 0 and self.k20 > 0 and self.k60 > 0 and self.k10 < self.k10_pre and self.k20 < self.k20_pre and self.k60 <= self.k60_pre:
+                self.log("SELL CREATE, %.2f" % self.data_close[0])
+                self.order = self.sell()
+
+    def log(self, txt, dt=None, do_print=False):  # 日志函数
+        if self.params.printlog or do_print:
+            dt = dt or self.datas[0].datetime.date(0)
+            print("%s, %s" % (dt.isoformat(), txt))
+
+    def notify_order(self, order):  # 记录交易执行情况
+        # 如果order为submitted/accepted,返回空
+        if order.status in [order.Submitted, order.Accepted]:
+            return
+        # 指令为buy/sell,报告价格结果
+        if order.status in [order.Completed]:
+            if order.isbuy():
+                self.log(
+                    f"买入:\n价格:{order.executed.price},\
+                成本:{order.executed.value},\
+                手续费:{order.executed.comm}"
+                )
+                self.buyprice = order.executed.price
+                self.buycomm = order.executed.comm
+            else:
+                self.log(
+                    f"卖出:\n价格：{order.executed.price},\
+                成本: {order.executed.value},\
+                手续费{order.executed.comm}"
+                )
+            self.bar_executed = len(self)
+
+        elif order.status in [order.Canceled, order.Margin, order.Rejected]:
+            self.log("交易失败")  # 指令取消/交易失败, 报告结果
+        self.order = None
+
+    def notify_trade(self, trade):  # 记录交易收益情况
+        if not trade.isclosed:
+            return
+        self.log(f"策略收益：\n毛收益 {trade.pnl:.2f}, 净收益 {trade.pnlcomm:.2f}")
+
+    def stop(self):  # 回测结束后输出结果
+        self.log(
+            "期末总资金 %.2f" % (self.broker.getvalue()),
+            do_print=True,
+            )
 
 
 class PandasDataPlus(bt.feeds.PandasData):
@@ -55,7 +107,7 @@ class PandasDataPlus(bt.feeds.PandasData):
     )
 
 if __name__ == "__main__":
-    code = "000977"  # 股票代码
+    code = "000737"  # 股票代码
     start_cash = 10000  # 初始自己为10000
     stake = 100  # 单次交易数量为1手
     commfee = 0.0005  # 佣金为万5
@@ -76,14 +128,12 @@ if __name__ == "__main__":
 
     # 将数据添加到引擎中
     cerebro.adddata(data)
-    # 设置初始资金
-    cerebro.broker.setcash(10000.00)
-    # 设置交易手续费,佣金为万5
-    cerebro.broker.setcommission(commission=0.0005)
-    # 添加性能分析器
-    cerebro.addanalyzer(btanalyzers.SharpeRatio, _name='mysharpe')
+    cerebro.addstrategy(SmaCross, printlog=True)
+    cerebro.addanalyzer(bt.analyzers.PyFolio, _name="PyFolio")
+    cerebro.broker.setcash(start_cash)  # broker设置资金
+    cerebro.broker.setcommission(commission=commfee)  # broker手续费
+    cerebro.addsizer(bt.sizers.FixedSize, stake=stake)  # 设置买入数量
     print("期初总资金: %.2f" % start_cash)
-    # 运行引擎
     back = cerebro.run()  # 运行回测
     end_value = cerebro.broker.getvalue()  # 获取回测结束后的总资金
     print("期末总资金: %.2f" % end_value)
