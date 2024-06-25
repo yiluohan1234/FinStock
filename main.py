6f7aba5e817597c7add71450fcc78b93b8535746
yiluohan1234 <1097189275@qq.com>
2024-06-25 21:21:41 +0800

    修改data数据

--- main.py
+++ main.py
@@ -14,7 +14,7 @@ from core.KLineChart import KLineChart
 
 def main(code):
     k = KLineChart(code, start_date="20230601")
-    k.plot(n=20, area=['V', 'M', 'KPL'], width=1060, height=900,
+    k.plot(n=20, area=['V', 'M', 'KPL'], width=1600, height=900,
            klines=['ma5', 'ma10', 'ma20', 'ma60', 'ma120', 'ma250'],
            vlines=['vma10', 'vma20'], dmalines=['ma10', 'ma60'],
            KLlines=['k10', 'k60'], KPLlines=['kp10', 'kp60'],
