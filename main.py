413f3d439d232e80f1595f7d68c5637d17a97af3
luojiahan <cuiyufei1234@126.com>
2024-06-27 08:42:35 +0800

    精简main代码

--- main.py
+++ main.py
@@ -10,14 +10,10 @@
 from core.KLineChart import KLineChart
 
 
-
-
 def main(code):
     k = KLineChart(code, start_date="20230601")
-    k.plot(n=20, area=['V', 'M', 'KPL'], width=1600, height=900,
+    k.plot(n=20, width=1600, height=900,
            klines=['ma5', 'ma10', 'ma20', 'ma60', 'ma120', 'ma250'],
-           vlines=['vma10', 'vma20'], dmalines=['ma10', 'ma60'],
-           KLlines=['k10', 'k60'], KPLlines=['kp10', 'kp60'],
            is_notebook=False
            )
     k.web()
@@ -25,17 +21,16 @@ def main(code):
 
 def main_a(code):
     k = KLineChart(code, zh_index=True)
-    k.plot(n=20, area=['V', 'M', 'KPL'], width=1600, height=900,
+    k.plot(n=20, width=1600, height=900,
            klines=['ma5', 'ma10', 'ma20', 'ma60', 'ma120', 'ma250'],
-           vlines=['vma10', 'vma60'],  dmalines=['ma10', 'ma60'],
-           KLlines=['k10', 'k60'], KPLlines=['kp10', 'kp60'],
            is_notebook=False
            )
     k.web()
 
 
 if __name__ == "__main__":
-    main_a("000001")
-    code_list = ["000737", "000977", "002948", "601877", "600595", "000612"]
-    for code in code_list:
-        main(code)
+    main("000977")
+    # main_a("000001")
+    # code_list = ["000737", "000977", "002948", "601877", "600595", "000612"]
+    # for code in code_list:
+    #     main(code)
