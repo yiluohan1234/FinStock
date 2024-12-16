4e9769da53ce81481b069a8cf7851b1aa7e1376d
yiluohan1234 <1097189275@qq.com>
2024-12-16 21:46:51 +0800

    update main.py

--- main.py
+++ main.py
@@ -12,7 +12,7 @@ from core.KLineChart import KLineChart
 
 def main(code):
     k = KLineChart(code, start_date="20230101", freq='D')
-    k.plot(n=20, width=1600, height=800, area=['V', 'M', 'KPL', 'MUL'], multiLines=['bias10', 'bias20', 'bias60'],
+    k.plot(n=20, width=1600, height=800, area=['V', 'M', 'KPL', 'MUL'], multiLines=['bias10', 'bias20', 'bias60', 'bias60-10'],
            klines=['ma5', 'ma10', 'ma20', 'ma60', 'ma120', 'ma250'], KPLlines=['kp10', 'kp20', 'kp60'],
            is_notebook=False
            )
@@ -52,6 +52,7 @@ if __name__ == "__main__":
     # main_industry("房地产开发") # 有色金属、计算机设备
     main_a("sh000852")
     main_a("000001")
-    code_list = ["000977", "000737", "603993", "002460", "601877", "002948", "600595", "000612"]
+    # code_list = ["000977", "000737", "603993", "002460", "601877", "002948", "600595", "000612"]
+    code_list = ["000977", "002948", "000612"]
     for code in code_list:
         main(code)
